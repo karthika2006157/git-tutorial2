1b0f5b9b8bd15343c9dfbd0c4a0138ba63024b70
karthikabalasubramani75@gmail.com <karthikabalasubramani75@gmail.com>
2025-06-19 11:37:07 +0530

    complete loope operator

---
+++ loops.js
@@ -0,0 +1,17 @@
+//for(let i=0;i<5;i++){
+    //console.log(i);
+//}
+
+let a=0;
+while(a<10){
+    if(a===5){
+        
+        a++;
+        continue;
+    }
+    if(a===8){
+        break;
+    }
+    console.log(a);
+    a++;
+}
