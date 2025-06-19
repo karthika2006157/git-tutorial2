9134f22e5909c913df6b827abaee015c4f45b936
karthikabalasubramani75@gmail.com <karthikabalasubramani75@gmail.com>
2025-06-19 20:56:36 +0530

    complete object

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ function add(a,b){
 let newObject={
     name: "John Doe",
     age:25,
+
     func:(a,b)=>{
         console.log(a,b);
     },
@@ -12,3 +13,28 @@ let newObject={
 console.log(newObject.name);
 console.log(newObject.age);
 newObject.func(2,3);
+
+function getDetails(){
+
+    let person={
+    name: "John Doe",
+    age:25,
+};
+return person;
+}
+let person={
+     name: "John Doe",
+    age:25,
+};
+
+let newDetails={
+    
+    ...person,
+    role:"developer",
+    company:"ABC pvt ltd",
+    isMarried:false,
+    salary:2.5
+};
+console.log(newDetails);
+
+   
