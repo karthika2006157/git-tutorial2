c101c7188a779f0d366b432e62c7d2bed6c96439
karthikabalasubramani75@gmail.com <karthikabalasubramani75@gmail.com>
2025-06-19 11:36:29 +0530

    complete conditional operators

---
+++ conditional.js
@@ -0,0 +1,16 @@
+let marks=85;
+if(marks>=90){
+    console.log("A grade");
+}
+else if(marks >=75){
+    console.log("B grade");
+}
+else{
+    console.log("C garde");
+}
+if(0){
+    console.log("Working");
+}
+else{
+    console.log("Falsy value");
+}
