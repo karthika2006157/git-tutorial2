e863d3038dc7e90a51cbb098fce13f85220fe94e
karthikabalasubramani75@gmail.com <karthikabalasubramani75@gmail.com>
2025-06-19 14:41:02 +0530

    complete objects topic

--- objects.js
+++ objects.js
@@ -1,7 +1,14 @@
+function add(a,b){
+    console.log(a+b);
+}
 let newObject={
-    name: "Hema",
-    age:20,
+    name: "John Doe",
+    age:25,
+    func:(a,b)=>{
+        console.log(a,b);
+    },
 
 };
 console.log(newObject.name);
 console.log(newObject.age);
+newObject.func(2,3);
